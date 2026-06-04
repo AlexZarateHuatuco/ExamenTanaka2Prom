9e72b76cf980d2cbcdf188599dedc3e2b8d451ea
D. Lezama <diavalez@gmail.com>
2026-06-03 21:28:10 -0500

    habitaciones

---
+++ Assets/Procedural/AnotherProceduralTest.cs
@@ -0,0 +1,69 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnotherProceduralTest : MonoBehaviour
+{
+    [Header("Configuración de Prefabs")]
+    [SerializeField] private List<GameObject> habitacionesPrefabs; 
+    [SerializeField] private Transform jugador;
+
+    [Header("Ajustes del Generador")]
+    [SerializeField] private int maxHabitacionesEnPantalla = 5;
+    [SerializeField] private float distanciaparaSpawn = 50f;
+
+    private List<GameObject> habitacionesActivas = new List<GameObject>();
+    private Vector3 puntoSiguienteSpawn;
+
+    private void Start()
+    {
+        puntoSiguienteSpawn = transform.position;
+
+        for (int i = 0; i < maxHabitacionesEnPantalla; i++)
+        {
+            GenerarHabitacion();
+        }
+    }
+
+    private void Update()
+    {
+        float distancia = Vector3.Distance(jugador.position, puntoSiguienteSpawn);
+
+        if (distancia < distanciaparaSpawn)
+        {
+            GenerarHabitacion();
+            BorrarHabitacionVieja();
+        }
+    }
+
+    private void GenerarHabitacion()
+    {
+        int indiceAleatorio = Random.Range(0, habitacionesPrefabs.Count);
+        GameObject prefabSeleccionado = habitacionesPrefabs[indiceAleatorio];
+        GameObject nuevaHabitacion = Instantiate(prefabSeleccionado, puntoSiguienteSpawn, Quaternion.identity);
+        habitacionesActivas.Add(nuevaHabitacion);
+
+        Transform entrada = nuevaHabitacion.transform.Find("Entrada");
+        Transform salida = nuevaHabitacion.transform.Find("Salida");
+
+        if (entrada != null && salida != null)
+        {
+            Vector3 desfaseEntrada = nuevaHabitacion.transform.position - entrada.position;
+            nuevaHabitacion.transform.position = puntoSiguienteSpawn + desfaseEntrada;
+            puntoSiguienteSpawn = salida.position;
+        }
+        else
+        {
+            puntoSiguienteSpawn += Vector3.forward * 10;
+        }
+    }
+
+    private void BorrarHabitacionVieja()
+    {
+        if (habitacionesActivas.Count > maxHabitacionesEnPantalla)
+        {
+            GameObject habitacionVieja = habitacionesActivas[0];
+            habitacionesActivas.RemoveAt(0);
+            Destroy(habitacionVieja);
+        }
+    }
+}
