21285371a34b8a14949634d4a89ee02feda0f9e7
Atzel7 <axelrojasb@hotmail.com>
2026-06-04 14:01:16 -0500

    Fixing code

--- Assets/Scripts/Weapons&Enemies/ShotgunShot.cs
+++ Assets/Scripts/Weapons&Enemies/ShotgunShot.cs
@@ -1,68 +1,3 @@
-//using UnityEngine;
-//using System.Collections;
-
-//public class ShotgunShot : WeaponBase
-//{
-//    [SerializeField] private Transform firePoint;
-//    [SerializeField] private GameObject pelletPrefab;
-//    [SerializeField] private int pelletCount = 8;
-//    [SerializeField] private float spread = 2f;
-
-//    private void Update()
-//    {
-//        HandleInput();
-//    }
-
-//    private void HandleInput()
-//    {
-//        if (IsReloading)
-//            return;
-
-//        if (Input.GetKeyDown(KeyCode.R))
-//        {
-//            StartCoroutine(Reload());
-//            return;
-//        }
-
-//        if (Input.GetMouseButtonDown(0))
-//        {
-//            Shoot();
-//        }
-//    }
-
-//    public override void Shoot()
-//    {
-//        if (!CanShoot())
-//            return;
-//        Debug.Log("Disparando shotgun");
-
-
-//        SetNextFireTime();
-//        ConsumeAmmo();
-
-//        for (int i = 0; i < pelletCount; i++)
-//        {
-//            SpawnPellet();
-//        }
-//    }
-
-//    private void SpawnPellet()
-//    {
-//        Vector3 direction = firePoint.forward;
-
-//        float randomX = Random.Range(-spread, spread) * 0.005f;
-//        float randomY = Random.Range(-spread, spread) * 0.005f;
-
-//        direction += firePoint.right * randomX;
-//        direction += firePoint.up * randomY;
-
-//        direction.Normalize();
-
-//        Vector3 spawnPos = firePoint.position + firePoint.forward * 0.2f;
-
-//        Instantiate(pelletPrefab, spawnPos, Quaternion.LookRotation(direction));
-//    }
-//}
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -128,8 +63,7 @@ public class ShotgunShot : WeaponBase
 
         foreach (var other in spawnedPellets)
         {
-            if (other != null)
-                Physics.IgnoreCollision(col, other);
+            if (other != null)Physics.IgnoreCollision(col, other);
         }
 
         spawnedPellets.Add(col);
